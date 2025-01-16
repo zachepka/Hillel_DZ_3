f3e094b863759f684c2b393ea3abe597736311b4
Vladyslav Zachepa <112680328+zachepka@users.noreply.github.com>
2025-01-16 00:20:41 -0330

    Changes. January 16/2025

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const ryadok1 = prompt('First ryadok:');
 const ryadok2 = prompt('Second ryadok:');
-const ryadok3 = prompt('Thrid ryadok:');
+const ryadok3 = prompt('Third ryadok:');
 
 if (ryadok1.trim() === '') {
     console.log("Pole ne moze buty poroznim!");
-}
-if (ryadok2.trim() === '') {
+} else if (ryadok2.trim() === '') {
     console.log("Pole ne moze buty poroznim!");
-}
-if (ryadok3.trim() === '') {
+} else if (ryadok3.trim() === '') {
     console.log("Pole ne moze buty poroznim!");
+} else {
+    const ryadky = [ryadok1, ryadok2, ryadok3];
+    console.log(`${ryadky[1]} ${ryadky[2]} ${ryadky[0]}`);
 }
-
-console.log(`${ryadok1}${ryadok2}${ryadok3}`);
